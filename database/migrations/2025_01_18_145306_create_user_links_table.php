94f7c5c23631178462a1296609dd5c41c8aa3ac6
ihor <ihor@ecaste.com>
2025-01-18 16:00:58 +0100

    Create tables, controllers and models

---
+++ database/migrations/2025_01_18_145306_create_user_links_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_links', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('description', 255)->nullable();
+            $table->text('image_path')->nullable();
+            $table->text('link_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_links');
+    }
+};
