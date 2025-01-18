7622324f11c444479940a614a1742b067bfc185a
ihor <ihor@ecaste.com>
2025-01-18 18:55:37 +0100

    Fileupload component

---
+++ app/View/Components/Forms/FileUpload.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\View\Components\Forms;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class FileUpload extends Component
+{
+    public $id;
+    public $url;
+    public $maxFiles;
+    public $maxFilesize;
+    public $singleton;
+    public $acceptedFiles;
+    public function __construct($id, $url, $maxFiles, $maxFilesize, $singleton, $acceptedFiles)
+    {
+        $this->id = $id;
+        $this->url = $url;
+        $this->maxFiles = $maxFiles;
+        $this->maxFilesize = $maxFilesize;
+        $this->singleton = $singleton;
+        $this->acceptedFiles = $acceptedFiles;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.forms.file-upload');
+    }
+}
