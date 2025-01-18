cf50b58fea8f99a486dec16a08f5816e860e6dc7
ihor <ihor@ecaste.com>
2025-01-18 18:55:58 +0100

    Create user links relation

--- app/Models/UserLink.php
+++ app/Models/UserLink.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserLink extends Model
 {
-    //
+    protected $table = 'user_links';
+
+    protected $fillable = [
+        'user_id',
+        'description',
+        'image_path',
+        'link_path',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
