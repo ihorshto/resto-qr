b472a98d3b39322cd7358689fb3933cd81472194
ihor <ihor@ecaste.com>
2025-01-18 18:55:10 +0100

    Create qrcode controller

--- app/Http/Controllers/UserQrCodeController.php
+++ app/Http/Controllers/UserQrCodeController.php
@@ -3,8 +3,74 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class UserQrCodeController extends Controller
 {
-    //
+
+    public function index(): View
+    {
+        return view('qrcode.index');
+    }
+
+    public function create()
+    {
+        // Show form for creating a new QR code
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Store a newly created QR code
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // Display a specific QR code
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // Show form for editing a QR code
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // Update a specific QR code
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // Delete a specific QR code
+    }
 }
