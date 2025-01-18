6fb39f01dfc9e973b8cf15a16b0d76dd6b6700e4
ihor <ihor@ecaste.com>
2025-01-18 18:52:46 +0100

    Import lodash and dropzone

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,13 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 import 'preline';
+import _ from 'lodash';
+import Dropzone from 'dropzone';
 
 window.Alpine = Alpine;
+
+// Expose Lodash and Dropzone to the global window object
+window._ = _;
+window.Dropzone = Dropzone;
 
 Alpine.start();
